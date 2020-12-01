6697ba2a46badd69576c5ccf3c7d813ac33cd5c2
kartik venkat <kartik.venkat86@gmail.com>
2020-12-01 08:02:58 -0500

    [task 1.3] Create main function. [main.cpp] Added main function.

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+
+/**
+ * @copyright (c) 2020, Kartik Venkat, Kushagra Agrawal, Aditya Khopkar
+ *
+ * @file main.cpp
+ * @authors
+ * Kartik Venkat kartik.venkat86@gmail.com \n
+ * Kushagra Agrawal kushagraagrawal425@gmail.com  \n
+ * Aditya Khopkar aadi0110@gmail.com  \n
+ *
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * MIT License
+ * @section DESCRIPTION:
+ * This is the main file to run the Gazebo demo..
+ */
+
+
+
+/*
+ * @brief main function
+ * @param argc
+ * @param argv
+ * @return None
+ */
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
