08922046e9d54431d9443c1c387a674b33bdc4fc
kushagra7176 <kushagraagrawal425@gmail.com>
2020-12-07 11:02:28 -0500

    [task 2.5] Create Level 1 Unit Tests. [detectorTest.cpp] Added Unit Test for detector node.

--- test/detectorTest.cpp
+++ test/detectorTest.cpp
@@ -1,27 +1,28 @@
 /**
+ *
+ * Copyright (c) 2020 Kartik Venkat Kushagra Agrawal Aditya Khopkar
+ *
+ * @section LICENSE
+ *
  * MIT License
-
-Copyright (c) 2020 Kartik Venkat Kushagra Agrawal Aditya Khopkar
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  * @file detectorTest.cpp
+ *
  * @authors
  * Kartik Venkat kartik.venkat86@gmail.com \n
  * Kushagra Agrawal kushagraagrawal425@gmail.com  \n
@@ -29,11 +30,8 @@ SOFTWARE.
  *
  * @version 1.0
  *
- * @section LICENSE
- *
- * MIT License
  * @section DESCRIPTION:
- * This is the unit test for the anomaly detector class.
+ * This is the unit test implementation for anomaly detector node.
  */
 
 #include <gtest/gtest.h>
