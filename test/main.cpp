a8bdc2abf721a6deecfb9a9b801b13ee9222db7a
kushagra7176 <kushagraagrawal425@gmail.com>
2020-12-07 11:03:45 -0500

    [task 2.5] Create Level 1 Unit Tests. [main.cpp] Added the main function to run rostests.

---
+++ test/main.cpp
@@ -0,0 +1,46 @@
+/**
+ *
+ * Copyright (c) 2020 Kartik Venkat Kushagra Agrawal Aditya Khopkar
+ *
+ * @section LICENSE
+ *
+ * MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @file main.cpp
+ *
+ * @authors
+ * Kartik Venkat kartik.venkat86@gmail.com \n
+ * Kushagra Agrawal kushagraagrawal425@gmail.com  \n
+ * Aditya Khopkar aadi0110@gmail.com  \n
+ *
+ * @version 1.0
+ *
+ * @section DESCRIPTION:
+ * This is the main file which runs the rostests.
+ */
+
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+
+// Run all the tests that were declared with TEST()
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    ros::init(argc, argv, "turtlebot_test");
+    ros::NodeHandle nh_;
+    return RUN_ALL_TESTS();
+}
