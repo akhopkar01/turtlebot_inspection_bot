9aac05c4f709e67703b54187d77480331c0eab73
akhopkar01 <aadi0110@gmail.com>
2020-12-14 00:45:42 -0500

    [task 3.6] Create Node Main file [src/detectorNode.cpp] Removed the main node action in main file

--- src/detectorNode.cpp
+++ src/detectorNode.cpp
@@ -35,26 +35,6 @@ int main(int argc, char* argv[]) {
   ros::NodeHandle n;
   // Node instance
   AnomalyDetector detector(n);
-  // Detection functionality
-  while (ros::ok()) {
-    // Checks if image is empty
-    if (detector.cvImg_.empty()) {
-      ROS_ERROR_STREAM("No callback received, Waiting..");
-    }
-    else {
-      detector.getImgPoints();
-      // If anomaly is detected
-      if (detector.anomalyDetected_) {       
-        cv::Point3f robotCoords = detector.localizePoints();
-        ROS_WARN_STREAM("Anomaly Detected at: " << robotCoords);
-      }
-      else {
-        ROS_INFO_STREAM("Exploring..");
-      }
-      cv::imshow("Turtlebot Viewer", detector.cvImg_);
-      cv::waitKey(3);
-    }
-    ros::spinOnce();
-  }
+  ros::spin();
   return 0;
 }
