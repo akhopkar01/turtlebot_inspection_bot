91dcb5d64621abde8d9d46f61d498e8225ac105e
akhopkar01 <aadi0110@gmail.com>
2020-12-14 00:18:33 -0500

    [task 3.6] Create Node Main file [src/detectorNode.cpp] Detection action in this file now

--- src/detectorNode.cpp
+++ src/detectorNode.cpp
@@ -26,12 +26,35 @@
  * @brief main function
  * @param argc
  * @param argv
- * @return None
+ * @return 0
  */
 
 int main(int argc, char* argv[]) {
+  // Create ROS node
   ros::init(argc, argv, "detector");
   ros::NodeHandle n;
+  // Node instance
   AnomalyDetector detector(n);
+  // Detection functionality
+  while (ros::ok()) {
+    // Checks if image is empty
+    if (detector.cvImg_.empty()) {
+      ROS_ERROR_STREAM("No callback received, Waiting..");
+    }
+    else {
+      detector.getImgPoints();
+      // If anomaly is detected
+      if (detector.anomalyDetected_) {       
+        cv::Point3f robotCoords = detector.localizePoints();
+        ROS_WARN_STREAM("Anomaly Detected at: " << robotCoords);
+      }
+      else {
+        ROS_INFO_STREAM("Exploring..");
+      }
+      cv::imshow("Turtlebot Viewer", detector.cvImg_);
+      cv::waitKey(3);
+    }
+    ros::spinOnce();
+  }
   return 0;
 }
