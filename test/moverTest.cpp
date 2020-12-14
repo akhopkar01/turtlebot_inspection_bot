6c391c525a68f359a93245535597e98b93cce145
kartik venkat <kartik.venkat86@gmail.com>
2020-12-14 17:53:21 -0500

    [task 3.9] Update Unit Tests Level 2 [moverTest.cpp] - Added additional Level 1 tests. - Added additional Level 2 tests. - Solved Cpplint and CppCheck errors.

--- test/moverTest.cpp
+++ test/moverTest.cpp
@@ -35,20 +35,22 @@
  */
 
 #include <gtest/gtest.h>
-#include <vector>
 #include <ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/LaserScan.h>
+#include <vector>
 #include <opencv2/opencv.hpp>
 #include <mover.hpp>
 
-TurtlebotMover turtlebot;
+
+
 /**
  * @ brief Test for checkObstacle method of the TurtlebotMover class.
  */
 TEST(Turtlebotmover, checkObstacle) {
+    TurtlebotMover turtlebot;
     bool obstacle = false;
     turtlebot.setObstacle(obstacle);
     EXPECT_FALSE(turtlebot.checkObstacle());
@@ -57,19 +59,70 @@ TEST(Turtlebotmover, checkObstacle) {
  * @ brief Test for changeDirection method of the class.
  */
 TEST(Turtlebotmover, changeDirection) {
+    TurtlebotMover turtlebot;
     std::string newDirection = "Right";
-    EXPECT_NO_FATAL_FAILURE(turtlebot.changeDirection(newDirection));
+    EXPECT_EQ(-0.8, turtlebot.changeDirection(newDirection));
+    newDirection = "Left";
+    EXPECT_EQ(+0.8, turtlebot.changeDirection(newDirection));
 }
 /**
  * @ brief Test for moveRobot method of the class.
  */
 TEST(Turtlebotmover, moveRobot) {
-EXPECT_NO_FATAL_FAILURE(turtlebot.moveRobot());
+    TurtlebotMover turtlebot;
+    EXPECT_NO_FATAL_FAILURE(turtlebot.moveRobot(true));
+    bool obstacle = true;
+    turtlebot.setObstacle(obstacle);
+    EXPECT_NO_FATAL_FAILURE(turtlebot.moveRobot(true));
 }
 
+/*
+ * @brief Test to check goalReached method of the class.
+ */
+TEST(Sensing, GoalReached) {
+    TurtlebotMover turtlebot;
+    geometry_msgs::Pose2D current_pose;
+    current_pose.x = 10;
+    current_pose.y = 10;
+    EXPECT_EQ(false, turtlebot.isGoalReached(current_pose));
+    current_pose.x = 1;
+    current_pose.y = 2;
+    EXPECT_EQ(true, turtlebot.isGoalReached(current_pose));
+}
+
+/*
+ * @brief LEVEL 2 ROS Test
+ */
 TEST(Sensing, LaserScan) {
-  ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>(
-      "/scan", 1000, &TurtlebotMover::scanEnvCallback, &turtlebot);
-  EXPECT_NO_FATAL_FAILURE(sub);
+TurtlebotMover turtlebot;
+ros::NodeHandle n;
+ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>(
+    "/scan", 1000, &TurtlebotMover::scanEnvCallback, &turtlebot);
+EXPECT_NO_FATAL_FAILURE(sub);
+}
+
+/*
+ * @brief LEVEL 2 ROS Test
+ */
+TEST(Sensing, pubVel) {
+    TurtlebotMover turtlebot;
+    ros::NodeHandle n;
+
+    // Initialize a Publisher that publishes velocity messages.
+    geometry_msgs::Twist velMsg;
+    ros::Publisher pubVel = n.advertise <geometry_msgs::Twist>
+        ("/cmd_vel", 1000);
+    velMsg.linear.x = 5.0;
+    velMsg.angular.z = 7.0;
+    pubVel.publish(velMsg);
+
+    /* Initialize a Subscriber that subscribes to
+       the published velocity messages.*/
+    ros::Subscriber getVel = n.subscribe<geometry_msgs::Twist>
+        ("/cmd_vel", 1000, &TurtlebotMover::velocityCallback, &turtlebot);
+    ros::spinOnce();
+
+    // Verify the published messages get updated.
+    EXPECT_EQ(5.0, turtlebot.linX);
+    EXPECT_EQ(7.0, turtlebot.angZ);
 }
